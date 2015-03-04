5ff950439beb9bee8234df577629103cb4773176
Siaxace <siaxace@siaxace-K53SD.(none)>
2015-03-04 11:56:55 +0330

    serverCore.cpp functions were implemented. next phase: testing

---
+++ include/core/serverCore.h
@@ -0,0 +1,33 @@
+#ifndef __SIAMOZ__serverCore
+#define __SIAMOZ__serverCore
+
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <ctime>
+#include <sstream>
+
+using namespace std;
+
+class ServerCoreClerk {
+	public:
+	void doClientCommand(string uname, string command);
+	private:
+	stringstream ss;
+	time_t startTime,endTime;
+	map<int,int> votes;
+	map<int,string> code2Name_Candidate;
+	vector<string> contributedClients;
+	string candidateNames();
+	string voteFor(string uname,int ccode);
+	void allResults();
+	void setVotingTime(int shour, int smin, int ehour, int emin);
+	void extendVotingTime(int ehour, int emin);
+	void addCandidate(string cname, int ccode);
+};
+
+
+
+#endif
